55caa4d4ba5930e3cea0d49229924c5cd87c97ba
shubham <ss.guwahati1996@gmail.com>
2025-01-03 15:54:55 +0530

    react update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Notification";
 
-ReactDOM.render(
-  <App text="All databases will be upgraded to support the latest features on Sunday. Please ensure you have a backup of your data." />,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <App text="All databases will be upgraded to support the latest features on Sunday. Please ensure you have a backup of your data." />
 );
